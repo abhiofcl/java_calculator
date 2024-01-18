7391676b8cd07787a15eb0d7ec1f7f239e62fc30
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2024-01-18 23:43:53 +0530

    added number buttons using JButton

--- Calculator.java
+++ Calculator.java
@@ -21,6 +21,54 @@ public class Calculator {
         displayLabel.setForeground(Color.white);
         jf.add(displayLabel);
 
+        JButton seven = new JButton("7");
+        seven.setBounds(30, 120, 80, 80);
+        jf.add(seven);
+
+        JButton eight = new JButton("8");
+        eight.setBounds(130, 120, 80, 80);
+        jf.add(eight);
+
+        JButton nine = new JButton("9");
+        nine.setBounds(230, 120, 80, 80);
+        jf.add(nine);
+
+        JButton four = new JButton("4");
+        four.setBounds(30, 230, 80, 80);
+        jf.add(four);
+
+        JButton five = new JButton("5");
+        five.setBounds(130, 230, 80, 80);
+        jf.add(five);
+
+        JButton six = new JButton("6");
+        six.setBounds(230, 230, 80, 80);
+        jf.add(six);
+
+        JButton one = new JButton("1");
+        one.setBounds(30, 330, 80, 80);
+        jf.add(one);
+
+        JButton two = new JButton("2");
+        two.setBounds(130, 330, 80, 80);
+        jf.add(two);
+
+        JButton three = new JButton("3");
+        three.setBounds(230, 330, 80, 80);
+        jf.add(three);
+
+        JButton dot = new JButton(".");
+        dot.setBounds(30, 430, 80, 80);
+        jf.add(dot);
+
+        JButton zero = new JButton("0");
+        zero.setBounds(130, 430, 80, 80);
+        jf.add(zero);
+
+        JButton equal = new JButton("=");
+        equal.setBounds(230, 430, 80, 80);
+        jf.add(equal);
+
     }
 
     public static void main(String[] args) {
